34ddc107cf10e76c582ded71df9b1775ba2590c4
Prabhu <prasadprabhu2222@gmail.com>
2023-06-10 09:27:16 +0530

    no change

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello! this the first line of code");
         System.out.println("Hello! this is the second line of code");
-        System.out.println(("Hello! this is the third line of code"));
     }
 }
